07c42fd50a9a014c57cc065629620634c3eb566c
hertz1 <mrdaniloazevedo@gmail.com>
2015-03-04 20:07:08 -0300

    Error Handling
    Added a error handling function.

---
+++ php/lib/error_handler.php
@@ -0,0 +1,8 @@
+<?php
+function errorHandler($errno, $errstr)
+{
+    $err = end(debug_backtrace());
+    echo "<b>Notice:</b> [".$errno."] ".$errstr." in ".$err['file']." on line ". $err['line'] ."<br>";
+}
+set_error_handler('errorHandler', E_USER_NOTICE);
+?>
