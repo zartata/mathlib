f0ec9dbd064d771c5a8e915c38bb054e40d4ebcc
hertz1 <mrdaniloazevedo@gmail.com>
2015-03-04 23:01:30 -0300

    Matrix functions
    Added matrix addition and subtraction functions.

---
+++ php/functions/matrixAddition.php
@@ -0,0 +1,31 @@
+<?php
+/*Calculates the subtraction of matrices of same size.*/
+
+require "../lib/error_handler.php";
+
+function matrix_addition()
+{
+    $args = func_get_args();
+    foreach ($args as $i => $arg) {
+        if (isset($args[$i+1])) {
+            if (count($arg) == count($args[$i+1])) {
+                foreach ($arg as $j => $array) {
+                    if (count($array) == count($args[$i+1][$j])) {
+                        foreach ($array as $k => $arr) {
+                            $return[$j][$k] = isset($return[$j][$k]) ? $return[$j][$k] + $args[$i+1][$j][$k] : $arr + $args[$i+1][$j][$k];
+                        }
+
+                    } else {
+                        trigger_error("Matrices must be same size", E_USER_NOTICE);
+                        exit();
+                    }
+                }
+
+            } else {
+                trigger_error("Matrices must be same size", E_USER_NOTICE);
+                exit();
+            }
+        }
+    }
+    return $return;
+}
